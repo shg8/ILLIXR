20bf1c3b6443fdf9cb4e13b1e40c9bca71efeaec
Henry Che <90811347+hungdche@users.noreply.github.com>
2022-03-05 18:35:38 -0600

    Running Unreal with ILLIXR

--- runner/runner/main.py
+++ runner/runner/main.py
@@ -111,7 +111,7 @@ def load_native(config: Mapping[str, Any]) -> None:
         ILLIXR_OFFLOAD_ENABLE=str(enable_offload_flag),
         ILLIXR_ALIGNMENT_ENABLE=str(enable_alignment_flag),
         ILLIXR_ENABLE_VERBOSE_ERRORS=str(config["enable_verbose_errors"]),
-        ILLIXR_RUN_DURATION=str(config["action"].get("ILLIXR_RUN_DURATION", 120)),
+        ILLIXR_RUN_DURATION=str(config["action"].get("ILLIXR_RUN_DURATION", 60)),
         ILLIXR_ENABLE_PRE_SLEEP=str(config["enable_pre_sleep"]),
         KIMERA_ROOT=config["action"]["kimera_path"],
         AUDIO_ROOT=config["action"]["audio_path"],
@@ -177,7 +177,7 @@ def load_tests(config: Mapping[str, Any]) -> None:
         env_override=dict(
             ILLIXR_DATA=str(data_path),
             ILLIXR_DEMO_DATA=str(demo_data_path),
-            ILLIXR_RUN_DURATION=str(config["action"].get("ILLIXR_RUN_DURATION", 120)),
+            ILLIXR_RUN_DURATION=str(config["action"].get("ILLIXR_RUN_DURATION", 10)),
             ILLIXR_OFFLOAD_ENABLE=str(enable_offload_flag),
             ILLIXR_ALIGNMENT_ENABLE=str(enable_alignment_flag),
             ILLIXR_ENABLE_VERBOSE_ERRORS=str(config["enable_verbose_errors"]),
@@ -289,8 +289,7 @@ def load_monado(config: Mapping[str, Any]) -> None:
         monado_service_proc = subprocess.Popen([str(monado_target_path)], env=env_monado_service, stdin=PIPE)
 
     ## Give the Monado service some time to boot up and the user some time to initialize VIO
-    print("|||||||||||||||||||||||||||START SLEEP---------------------------------")
-    #time.sleep(5)
+    time.sleep(5)
 
 
     subprocess_run(
